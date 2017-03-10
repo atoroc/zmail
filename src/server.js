4f3dabe7872a9b388a4562357c7146ff207e4bb6
David Hemphill <davidlee.hemphill@gmail.com>
2017-03-10 14:38:23 -0600

    Run server.js through prettier and extract request data to their own props instead of data

--- src/server.js
+++ src/server.js
@@ -1,39 +1,46 @@
-import _ from 'lodash'
-import Oy from 'oy-vey'
-import React from 'react'
-import emails from './emails'
-import express from 'express';
-import bodyParser from 'body-parser'
-import writeFile from './lib/writeFile'
-import generateCustomTemplate from './lib/generateCustomTemplate'
+import _ from "lodash";
+import Oy from "oy-vey";
+import React from "react";
+import emails from "./emails";
+import express from "express";
+import bodyParser from "body-parser";
+import writeFile from "./lib/writeFile";
+import generateCustomTemplate from "./lib/generateCustomTemplate";
 
 const server = express();
-server.set('port', (process.env.PORT || 8887));
+server.set("port", process.env.PORT || 8887);
 
-server.use(bodyParser.json());       // to support JSON-encoded bodies
-server.use(bodyParser.urlencoded({     // to support URL-encoded bodies
-  extended: true
-}));
+server.use(bodyParser.json()); // to support JSON-encoded bodies
 
-server.get('/:template.:type', (req, res) => {
-  const email = _.find(emails, (email) => {
-    return email.fileName == req.params.template + '.html'
-  })
+server.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  }),
+);
 
-  const {query, body} = req
-  const data = Object.assign({}, query, body)
+server.get("/:template.:type", (req, res) => {
+  const email = _.find(emails, email => {
+    return email.fileName == req.params.template + ".html";
+  });
+
+  const { query, body } = req;
+  const data = Object.assign({}, query, body);
 
   if (email) {
-      const html = Oy.renderTemplate(<email.component data={data} />, email.options, (templateOptions) => generateCustomTemplate(templateOptions))
+    const html = Oy.renderTemplate(
+      <email.component {...data} />,
+      email.options,
+      templateOptions => generateCustomTemplate(templateOptions),
+    );
 
-      res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
-      res.set('Pragma', 'no-cache');
-      res.set('Expires', '0');
-      res.send(html);
+    res.set("Cache-Control", "no-cache, no-store, must-revalidate");
+    res.set("Pragma", "no-cache");
+    res.set("Expires", "0");
+    res.send(html);
   }
-
 });
 
-server.listen(server.get('port'), () => {
-  console.log('Node server is running on port', server.get('port'));
+server.listen(server.get("port"), () => {
+  console.log("Node server is running on port", server.get("port"));
 });
