0168a742dd81b8f51cf09a561537427a0e3b44a3
David Hemphill <davidlee.hemphill@gmail.com>
2017-03-14 14:19:36 -0500

    remove `px` from width and height attributes

--- src/components/social.js
+++ src/components/social.js
@@ -18,16 +18,16 @@ export default (props) => {
                     <td>
                         <p style={{textAlign: 'center', margin: 0, padding: 0}}>
                             <a style={linkStyles} href="http://instagram.com/lumi" target="_blank">
-                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-insta-bg2.gif" width="30px" height="30px" />
+                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-insta-bg2.gif" width="30" height="30" />
                             </a>
                             <a style={linkStyles} href="http://facebook.com/lumi" target="_blank">
-                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-fb-bg2.gif" width="30px" height="30px" />
+                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-fb-bg2.gif" width="30" height="30" />
                             </a>
                             <a style={linkStyles} href="http://twitter.com/lumi" target="_blank">
-                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-twitter-bg2.gif" width="30px" height="30px" />
+                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-twitter-bg2.gif" width="30" height="30" />
                             </a>
                             <a href="http://pinterest.com/lumi" target="_blank">
-                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-pinterest-bg2.gif" width="30px" height="30px" />
+                                <img src="https://s3.amazonaws.com/lumi-assets/social-60-pinterest-bg2.gif" width="30" height="30" />
                             </a>
                         </p>
                     </td>
